98addbbfa645241ad34ded3788ff940037b0e7dc
tizianoanicolas <tizianoanicolas@gmail.com>
2026-06-05 15:51:16 -0300

    login_hecho

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Login from "./pages/Login";
 
 function App() {
-  return <Login />;
+  return (
+    <BrowserRouter>
+
+      <Routes>
+
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+
+      </Routes>
+
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,21 +1,66 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import api from "../services/api";
 import "../styles/login.css";
 
 function Login() {
+  const [correo, setCorreo] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const navigate = useNavigate();
+
+  const manejarLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await api.post("/login", {
+        Correo: correo,
+        Password: password,
+      });
+
+      localStorage.setItem("token", response.data.token);
+      localStorage.setItem("rol", response.data.rol);
+
+      navigate("/home");
+    } catch (err) {
+      setError("Correo o contraseña incorrectos");
+    }
+  };
+
   return (
     <div className="login-container">
       <div className="login-box">
         <h1>Iniciar Sesión</h1>
 
-        <form>
+        <form onSubmit={manejarLogin}>
           <input
             type="email"
             placeholder="Correo electrónico"
+            value={correo}
+            onChange={(e) => {
+              setCorreo(e.target.value);
+              setError("");
+            }}
+            required
           />
 
           <input
             type="password"
             placeholder="Contraseña"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+              setError("");
+            }}
+            required
           />
+
+          {error && (
+            <p className="error">
+              {error}
+            </p>
+          )}
 
           <button type="submit">
             Ingresar
